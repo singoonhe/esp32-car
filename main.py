355871130c0c830cda819f9be7a27c8146167afc
heguanghui <heguanghui@moqihuyu.com>
2024-01-20 16:57:58 +0800

    修改network文件名，避免与系统冲突

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 #入口功能
-import machine
 import time
-
-print(5+2)
-print(machine.freq())
-
-import os
-print(os.listdir())
-
 import camera
+import _thread
 import cam_config as cc
-# set camera configuration
-cc.configure(camera, cc.ai_thinker)
-camera.conf(cc.PIXFORMAT,cc.PIXFORMAT_RGB565) # both pixformat and 
-camera.conf(cc.FRAMESIZE,cc.FRAMESIZE_QQVGA) # framesize MUST before camera.init
-camera.init()
-# other setting after init
-camera.quality(12)
-img=camera.capture()
-print(len(img))
-time.sleep(3)
-img=camera.capture()
-print(len(img))
+from machine import Timer
+from wifi import wifi_network
 
+# 摄像头截图循环
+def camera_loop(wifi, camera):
+    while True:
+        if wifi.is_ready():
+            img=camera.capture()
+        else:
+            time.sleep_ms(100)
+
+# 初始化摄像头
+def init_camera(wifi):
+    # set camera configuration
+    cc.configure(camera, cc.ai_thinker)
+    camera.conf(cc.PIXFORMAT,cc.PIXFORMAT_RGB565) # both pixformat and 
+    camera.conf(cc.FRAMESIZE,cc.FRAMESIZE_QQVGA) # framesize MUST before camera.init
+    camera.init()
+    # other setting after init
+    camera.quality(12)
+#     _thread.start_new_thread(camera_loop, (wifi, camera))
+
+# 定时调用
+def wheel_timer():
+    pass
+
+if __name__ == '__main__':
+    wifi = wifi_network()
+    if wifi.init():
+        init_camera(wifi)
+        # 电机控制定时器
+        while True:
+            pass
+#         tim = Timer(-1)
+#         tim.init(period=2000, mode=Timer.PERIODIC, callback=wheel_timer())
--- wifi.py
+++ wifi.py
@@ -11,7 +11,7 @@ import machine
 WIFI_AP    = const(0) # 热点
 WIFI_STA   = const(1) # 连接到路由
 
-class car_network():
+class wifi_network():
     # 初始化方法
     def __init__(self):
         self.sys_config = {}
@@ -28,17 +28,17 @@ class car_network():
         self.ip = '0'
         self.udp_socket = None
         self.connect_addr = None
-        self.init_wifi()
         
     # 初始化网络
-    def init_wifi(self):
+    def init(self):
         if self.sys_config['wifi_type'] == WIFI_STA:
             sta_if = network.WLAN(network.STA_IF)
             # 网络可能已经连接
             if not sta_if.isconnected():
                 sta_if.active(True)
                 try:
-                    sta_if.connect('HUAWEI-HF', 'HF123456')
+#                     sta_if.connect('HUAWEI-HF', 'HF123456')
+                    sta_if.connect('xingqiwan', 'xingqiwanWifi')
                     while not sta_if.isconnected():
                         time.sleep_ms(100)
                     self.ip = sta_if.ifconfig()[0]
@@ -52,14 +52,17 @@ class car_network():
                 self.ip = ap_if.ifconfig()[0]
             except:
                 print("Network ap failed, reset system")
-                machine.reset()
         # 添加网络线程
         if len(self.ip) > 1:
-            _thread.start_new_thread(self.recv_data, (self))
+            _thread.start_new_thread(self.recv_data, ())
+            return True
+        else:
+            return False
             
     # 开始接受网络连接
     def recv_data(self):
         try:
+            print("current ip : " + self.ip)
             self.udp_socket = socket(AF_INET, SOCK_DGRAM)
             self.udp_socket.bind((self.ip, 7890))
             while True: #接收数据receiving data
@@ -82,8 +85,12 @@ class car_network():
             
     # 发送指定的数据
     def send_data(self, data):
-        if self.connect_addr and self.udp_socket:
+        if self.is_ready():
             self.udp_socket.sendto(data)
+            
+    # 接收者是否已准备好
+    def is_ready():
+        return self.connect_addr != None and self.udp_socket != None
             
     # 保存配置文件
     def save_config(self):
@@ -93,5 +100,3 @@ class car_network():
             file.close()
         except OSError:  # open failed
             print("Save config failed!")
-
-car = car_network()
