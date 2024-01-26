94e3ca99f3b639cd8798cf94dd04ef96d9aea7a9
heguanghui <heguanghui@moqihuyu.com>
2024-01-26 18:42:18 +0800

    修改与microbit通信协议 为uart

--- main_micro.py
+++ main_micro.py
@@ -1,6 +1,6 @@
 # ESP32-C3通过microbit通信
 import time
-from machine import Timer,Pin,SoftI2C
+from machine import Timer,Pin,UART
 from wifi import wifi_network
 from data import network_data
 
@@ -10,10 +10,8 @@ command_target = None
 network_wifi = None
 # 网络检查，未收到数据的计数
 network_check = 0
-# i2c通信对象
-micro_i2c = None
-# i2c通信地址
-micro_addr = None
+# uart通信对象
+micro_uart = None
 
 # 接收到命令数据
 def recv_command_data(data, addr):
@@ -33,22 +31,22 @@ def recv_command_data(data, addr):
                 # 发送移动方向
                 move_dir = int(move_info[0])
                 if move_dir > 315 and move_dir <= 45:
-                    send_i2c_byte(11)
+                    send_micro_byte(11)
                 elif move_dir > 45 and move_dir <= 135:
-                    send_i2c_byte(12)
+                    send_micro_byte(12)
                 elif move_dir > 135 and move_dir <= 225:
-                    send_i2c_byte(13)
+                    send_micro_byte(13)
                 elif move_dir > 225 and move_dir <= 315:
-                    send_i2c_byte(14)
+                    send_micro_byte(14)
                 else: # move_dir == -1
-                    send_i2c_byte(0)
+                    send_micro_byte(0)
                 # 发送速度
-                send_i2c_byte(int(move_info[1]))
+                send_micro_byte(int(move_info[1]))
 
-# 发送i2c数据
-def send_i2c_byte(i2c_byte):
-    if micro_addr != None:
-        micro_i2c.writeto(micro_addr, bytes([i2c_byte]))
+# 发送uart数据
+def send_micro_byte(s_byte):
+    if micro_uart != None:
+        micro_uart.write(bytes([s_byte]))
     
 # 发送命令数据
 def send_command_data(cmd_type, cmd_value=None):
@@ -69,14 +67,8 @@ def check_wifi_target(t):
         print('clear command target because of time out')
 
 if __name__ == '__main__':
-    # 初始化通信i2c
-    micro_i2c = SoftI2C(scl=Pin(12), sda=Pin(12), freq=100000)
-    i2c_devices = micro_i2c.scan()
-    if len(i2c_devices) > 0:
-        micro_addr = i2c_devices[0]
-    else:
-        print('cannot find i2c device')
-        return 0
+    # 初始化通信uart
+    micro_uart = UART(0, 115200)
     # 开启网络超时检查
     wifi_timer = Timer(-1)
     wifi_timer.init(period=500, mode=Timer.PERIODIC, callback=check_wifi_target)
