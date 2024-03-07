0a8ff7b5e435c03d56f13c6b8c074f5bf804cd58
singoon.he <singoon.he@gmail.com>
2024-03-07 22:02:30 +0800

    单边运行时独立计算占空比，需要更快的速度

--- wheel_pwm.py
+++ wheel_pwm.py
@@ -25,8 +25,13 @@ class wheel_pwm:
         self.pwm_pins = []
         # 初始化各速度下的空占比
         self.speed_dutys = [0]
+        self.channel_dutys = [0]
         for i in range(10):
             self.speed_dutys.append(min(1023, DEFAULT_MIN_DUTY + DEFAULT_DUTY_STEP * i))
+            # 单边运行时的速度，往往需要更大的空占比
+            self.channel_dutys.append(min(1023, DEFAULT_MIN_DUTY * 2 + DEFAULT_DUTY_STEP * i))
+        # 当前是单边还是双边运行
+        self.run_two_channel = True
         # 初始化各速度下的期望转速值
         self.speed_counts = [0]
         speed_count_step = (MAX_SPEED_COUNT - MIN_SPEED_COUNT) * 0.1
@@ -87,12 +92,14 @@ class wheel_pwm:
             # 当前方向为0，停止移动
             self.remote_speed[0] = 0
             self.remote_speed[1] = 0
+            self.run_two_channel = True
         elif move_dir > 225 and move_dir < 315:
             # 左右一起倒车
             value1 = 0
             value2 = 1
             self.remote_speed[0] = speed
             self.remote_speed[1] = speed
+            self.run_two_channel = True
         else:
             value1 = 1
             value2 = 0
@@ -104,6 +111,8 @@ class wheel_pwm:
             # 设置左右轮的速度值(0~10)
             self.remote_speed[0] = max(0, min(int((180-move_dir) / step_angle), speed))
             self.remote_speed[1] = max(0, min(int(move_dir / step_angle), speed))
+            # 标记是单边还是双边运行
+            self.run_two_channel = (self.remote_speed[0] != 0 and self.remote_speed[1] != 0)
         # 设置双侧电机初始值
         for i in range(2):
             self.set_pin_values(i, value1, value2, self.remote_speed[i], from_static)
@@ -115,17 +124,20 @@ class wheel_pwm:
         self.nor_pins[index * 2 + 1].value(value2)
         # 设置电机的速度, 缓存判断避免多次设置
         if self.last_remote_s[index] != pwm_v:
+            duty_list = self.run_two_channel and self.speed_dutys or self.channel_dutys
             if from_static:
                 # 从静止启动时，给定启动空占比不能太低
-                self.pwm_pins[index].duty(max(self.speed_dutys[pwm_v], MIN_START_DUTY))
+                self.pwm_pins[index].duty(max(duty_list[pwm_v], MIN_START_DUTY))
             else:
-                self.pwm_pins[index].duty(self.speed_dutys[pwm_v])
+                self.pwm_pins[index].duty(duty_list[pwm_v])
             self.last_remote_s[index] = pwm_v
             # 重置测速的次数
             self.speed_irq_count = 0
             
     # 定时检测当前速度值是否合理，并修正
     def check_count_speed(self, t):
+        # 判断当前应该使用双边还是单边速度
+        duty_list = self.run_two_channel and self.speed_dutys or self.channel_dutys
         for i in range(2):
             # 略过初始的前2次测速，刚启动时测速不准确
             self.speed_irq_count += 1
@@ -134,7 +146,7 @@ class wheel_pwm:
                 # 同时重置以前的缓存速度，让最小速度只生效一次
                 for j in range(2):
                     self.irq_counts[j] = 0
-                    cache_duty = self.speed_dutys[self.remote_speed[j]]
+                    cache_duty = duty_list[self.remote_speed[j]]
                     self.pwm_pins[j].duty(cache_duty)
                 break
             
@@ -145,11 +157,14 @@ class wheel_pwm:
             count_diff = self.speed_counts[remote_speed] - count_s
             # 电机空占比修正
             fix_ratio = max(-1, min(1, count_diff / DUTY_DIFF_STEP))
-            fix_duty = self.speed_dutys[remote_speed] + int(fix_ratio * 50)
-            fix_duty = max(0, min(fix_duty, 1023))
+            fix_duty = duty_list[remote_speed] + int(fix_ratio * 50)
+            # 下一级的占空比
+            next_duty = duty_list[min(10, remote_speed + 1)]
+            fix_duty = max(0, min(fix_duty, next_duty))
+            # 修正占比不能超过下一级占比，避免占比陡增
             self.pwm_pins[i].duty(fix_duty)
             # 修正值存储起来下次继续使用
-            self.speed_dutys[remote_speed] = fix_duty
+            duty_list[remote_speed] = fix_duty
             self.irq_counts[i] = 0
 #             print(count_s, count_diff, fix_duty, remote_speed)
         
